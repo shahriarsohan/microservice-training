f7ef591e8241458f3495086effc01b040d8f4e6f
shahriarsohan <sohanmock@gmail.com>
2022-03-02 13:41:04 +0600

    skaffold and nginx-ingree added

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,6 +5,10 @@ import { json } from "body-parser";
 const app = express();
 app.use(json());
 
+app.get("/api/users/currentuser", (req, res) => {
+  res.send("Hi there");
+});
+
 app.listen(3000, () => {
-  console.log("Listening on Port 3000");
+  console.log("Listening on PORT 3000");
 });
