dbd816a0039987f80f45f8766ead49a737865247
shahriarsohan <sohanmock@gmail.com>
2022-03-30 00:20:36 +0600

    authentication work done and product model start

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,14 +1,49 @@
 import express from "express";
 
 import { json } from "body-parser";
+import mongoose from "mongoose";
+import cookieSession from "cookie-session";
+
+import { currentUserRouter } from "./routes/currentuser";
+import { signIn } from "./routes/signin";
+import { signOut } from "./routes/signout";
+import { signupRouter } from "./routes/signup";
+import { errorHandler } from "./middlewares/error-handler";
+import { NotFoundError } from "./errors/not-found-error";
 
 const app = express();
+app.set("trust proxy", true);
 app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true,
+  })
+);
 
-app.get("/api/users/currentuser", (req, res) => {
-  res.send("Hi there");
+app.use(currentUserRouter);
+app.use(signIn);
+app.use(signOut);
+app.use(signupRouter);
+
+app.all("*", async (req, res) => {
+  throw new NotFoundError();
 });
+
+app.use(errorHandler);
+
+const start = async () => {
+  try {
+    await mongoose.connect("mongodb://auth-mongo-svc:27017/auth");
+
+    console.log("Connected to MongoDB");
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 app.listen(3000, () => {
-  console.log("Listening on PORT 3000");
+  console.log("Listening on port 3000");
 });
+
+start();
